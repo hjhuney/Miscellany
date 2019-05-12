a70a839572c3b4eca42179699f91ad6075f2778f
Jake Huneycutt <hjhuney@gmail.com>
2019-05-12 18:02:43 -0400

    Add files via upload

--- horse_algo.py
+++ horse_algo.py
@@ -1,9 +1,21 @@
+import sys
+
+n = 20
+test1 = 'miss miss hit miss hit miss hit miss miss hit miss miss miss miss hit hit hit miss hit miss'
+# print(len(test1))
+
+# num = int(input())
+# test = input()
+
+shots = test1.split()
+# print(shots)
+
 horse = "HORSE"
 counter = 0
 p1_misses = 0
 p2_misses = 0
 
-print(len(shots))
+# print(len(shots))
 
 while counter < len(shots):
     if shots[counter] == "hit":
